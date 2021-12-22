7fe853c7f8acbdf370ed6a8b6af73c854ef1709b
Dmitry <dm.rubashko@gmail.com>
2021-12-23 01:24:11 +0300

    description

--- app/src/pages/resultPage/components/resultPageLayout/index.js
+++ app/src/pages/resultPage/components/resultPageLayout/index.js
@@ -1,36 +1,39 @@
 import {useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 import {ROUTES} from "../../../../routes/routesNames";
-import styles from "../../../../commonComponents/header/styles.module.scss";
+import styles from "./styles.module.scss"
 import Button from "@mui/material/Button";
 
 const ResultPageLayout = ({}) => {
 
   const {info, isLoading, isError, error} = useSelector(state => state.word);
 
-
   return (
     <>
-      <div>
+      <div className={styles.button}>
         <Link to={ROUTES.HOME_PAGE} className={styles.link}>
           <Button style={{fontSize: '20px', backgroundColor: 'rgb(100,88,95)', color: '#9a782f', fontFamily: 'Architects Daughter, cursive'}}>Back To Home Page</Button>
         </Link>
       </div>
-      <div>
+      <div className={styles.props}>
         {isError ? error.response :
         <>
-          <p>{info[0].word}</p>
-          <p>{info[0].phonetics[0].text}</p>
-          <p>{info[0].phonetics[0].audio}</p>
-          <p>{info[0].phonetics[1].text}</p>
-          <p>{info[0].phonetics[1].audio}</p>
-          <p>{info[0].meanings[0].partOfSpeech}</p>
-          <p>{info[0].meanings[0].definitions[0].definition}</p>
-          <p>{info[0].meanings[0].definitions[0].example}</p>
-          <p>{info[0].meanings[1].partOfSpeech}</p>
-          <p>{info[0].meanings[1].definitions[0].definition}</p>
-          <p>{info[0].meanings[1].definitions[0].example}</p>
-          {/*<p>{info[0].meanings[1].definitions[0].synonyms.join(',')}</p>*/}
+          <p className={styles.description}>Word: <span className={styles.word}>{info[0].word}</span></p>
+          <p className={styles.description}>Transcription: {info[0].phonetics[0]?.text}</p>
+          <p className={styles.description}>Pronunciation:</p>
+          <audio src={info[0].phonetics[0]?.audio} controls></audio>
+          {/*<p>{info[0].phonetics[1]?.text}</p>*/}
+          {/*<audio src={info[0].phonetics[1]?.audio} controls></audio>*/}
+          <p className={styles.description}>Part Of Speech: {info[0].meanings[0]?.partOfSpeech}</p>
+          <p className={styles.description}>{info[0].meanings[0]?.definitions[0].definition}</p>
+          <p className={styles.description}>{info[0].meanings[0]?.definitions[0].example}</p>
+          <p className={styles.description}>{info[0].meanings[1]?.partOfSpeech}</p>
+          <p className={styles.description}>{info[0].meanings[1]?.definitions[0].definition}</p>
+          <p className={styles.description}>{info[0].meanings[1]?.definitions[0].example}</p>
+          <p className={styles.description}>{info[0].meanings[1]?.definitions[0].synonyms.join(', ')}</p>
+          <p className={styles.description}>{info[0].meanings[2]?.partOfSpeech}</p>
+          <p className={styles.description}>{info[0].meanings[2]?.definitions[0].definition}</p>
+          <p className={styles.description}>{info[0].meanings[2]?.definitions[0].example}</p>
         </>
         }
       </div>
